b60dcce4a787e2fd5aa6b9f8c0b07f2a7f381a29
Skrit <skrityx@gmail.com>
2022-07-04 01:49:36 +0300

    add current command, refactor

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,111 @@
 use std::env;
-use std::process;
 use registry::{Hive, Security};
+
+
+const MHY_KEY: &str = r"SOFTWARE\miHoYo\Genshin Impact";
+const MHY_REG: &str = "MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810";
+
+
+const SESSIONS_KEY: &str = r"SOFTWARE\miHoYo\Genshin Impact\sessions";
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // if args.len() < 2 {
-    //     println!("No command given. Exiting...");
-    //     process::exit(1);
-    // }
-	let session = read_session();
-	//println!("The current session is: {:?}", session);
-    match args.len() {
-        // 2=>match args[1].as_str() {
-            // "current"=>println!("One"),
-            // _=>{    println!("Expected 1-2 arguments, received {}",args.len()-1);
-                    // process::exit(1); },
-        // },
-        3=>match args[1].as_str() {
-            "current"=>current(session),
-            "save"=>save(&args[2],session),
-            "delete"=>delete(&args[2]),
-            "run"=>println!("run"),
-            "load"=>load(&args[2]),
-            _=>println!("Command not recognized. Exiting..."),
-        },
-        1|_=>{  println!("Expected 1-2 arguments, received {}",args.len()-1);
-                process::exit(1); },
-    }
-}
+
+    check_sessionstore();
     
-
-fn read_session() -> registry::Data {
-	
-    if let Ok(reg) = Hive::CurrentUser.open(r"SOFTWARE\miHoYo\Genshin Impact", Security::Read) {
-		if let Ok(session) = reg.value("MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810") {
-			session
-		} else {
-			println!("No session found. Try logging in to Genshin Impact. Terminating...");
-			process::exit(1);
-		}
-    } else {
-        println!("No session found. Try logging in to Genshin Impact. Terminating...");
-        process::exit(1);
+	//println!("The current session is: {:?}", session);name: Option<&str>
+    let (command, name): (&str, Option<&str>);
+    match args.len() {
+        2 => { { (command, name) = (args[1].as_str(), None); } },
+        3 => { (command, name) = (args[1].as_str(), Some(args[2].as_str())); },
+        _ => { panic!("Expected 1-2 arguments, received {}", args.len()-1); },
+    }
+    
+    match command {
+        "current"=>current(),
+        "save"=>save(handle_name(name)),
+        "delete"=>delete(handle_name(name)),
+        //"run"=>println!("run"),
+        "load"=>load(handle_name(name)),
+        _=> panic!("Command not recognized. Exiting..."),
     }
 }
 
-
-fn read_session_from_store(name : &String) -> registry::Data {
-	
-    if let Ok(reg) = Hive::CurrentUser.open(r"SOFTWARE\miHoYo\Genshin Impact\sessions", Security::Read) {
-		if let Ok(session) = reg.value(name) {
-			session
-		} else {
-			println!("No session found in store. Terminating...");
-			process::exit(1);
-		}
-    } else {
-        println!("No session found in store. Terminating...");
-        process::exit(1);
+fn handle_name(name: Option<&str>) -> &str {
+    match name {
+        Some(name) => name,
+        None => panic!("This command requires an argument"),
     }
 }
 
+// Creates Address Tuple, 1 &str is registry key, second is name
+fn select_key(name: Option<&str>) -> (&str, &str) {
+    match name {
+        Some(x) => { (SESSIONS_KEY, x) },
+        None => { (MHY_KEY, MHY_REG) },
+    }
+}
+
+// Takes session name or None, returns Data read
+fn read_session(name: Option<&str>) -> registry::Data {
+	let addr = select_key(name);
+
+    if let Ok(reg) = Hive::CurrentUser.open(addr.0, Security::Read) {
+		if let Ok(session) = reg.value(addr.1) {
+			session
+		} else {
+			panic!("No session found. Try logging in to Genshin Impact. Terminating...");
+		}
+    } else {
+        panic!("Cannot read sessions key. Try logging in to Genshin Impact. Terminating...");
+    }
+}
+
+fn write_session(data: registry::Data, name: Option<&str>) {
+	let addr = select_key(name);
+	if let Ok(reg) = Hive::CurrentUser.open(addr.0, Security::Write) {
+		println!("Result: {:?}",reg.set_value(addr.1, &data));
+	}
+}
 
 fn check_sessionstore() {
-    if let Err(_) = Hive::CurrentUser.open(r"SOFTWARE\miHoYo\Genshin Impact\sessions", Security::Read) {
-        if let Err(_) = Hive::CurrentUser.create(r"SOFTWARE\miHoYo\Genshin Impact\sessions", Security::Read) {
-			println!("Couldn't create sessions key. Terminating...");
+    if let Err(_) = Hive::CurrentUser.open(SESSIONS_KEY, Security::Read) {
+        if let Err(_) = Hive::CurrentUser.create(SESSIONS_KEY, Security::Read) {
+			panic!("Couldn't create sessions key. Terminating...");
 		}
     }
 }
 
-
-fn save(name : &String, session_data : registry::Data) {
-	check_sessionstore();
+fn save(name : &str) {
     println!("saving {}...",name);
-	if let Ok(reg) = Hive::CurrentUser.open(r"SOFTWARE\miHoYo\Genshin Impact\sessions", Security::Write) {
-		println!("Result: {:?}",reg.set_value(name, &session_data));
-	}
+	write_session(read_session(None), Some(name))
 }
 
-fn load(name : &String) {
-	check_sessionstore();
-	let session_data = read_session_from_store(name);
+fn load(name : &str) {
     println!("loading {}...",name);
-	if let Ok(reg) = Hive::CurrentUser.open(r"SOFTWARE\miHoYo\Genshin Impact", Security::Write) {
-		println!("Result: {:?}",reg.set_value("MIHOYOSDK_ADL_PROD_OVERSEA_h1158948810", &session_data));
-	}
+	write_session(read_session(Some(name)), None)
 	
 }
 
-fn delete(name : &String) {
-	check_sessionstore();
-	let session_data = read_session_from_store(name);
+fn delete(name : &str) {
     println!("deleting {}...",name);
-	if let Ok(reg) = Hive::CurrentUser.open(r"SOFTWARE\miHoYo\Genshin Impact\sessions", Security::Write) {
+	if let Ok(reg) = Hive::CurrentUser.open(SESSIONS_KEY, Security::Write) {
 		println!("Result: {:?}",reg.delete_value(name));
 	}
 	
 }
 
-fn current(session_data : registry::Data) {
-    println!("currenting...");
-	if let Ok(reg) = Hive::CurrentUser.open(r"SOFTWARE\miHoYo\Genshin Impact\sessions", Security::Read) {
-		for (val, key) in reg.values().zip(reg.keys()) {
-				println!("Key: {:?},", key);
-			if let Ok(val) = session_data {
-				println!("Current session is in store under the name: {:?}", key);
-			}
+fn current() {
+    let session_data = &read_session(None);
+	if let Ok(reg) = Hive::CurrentUser.open(SESSIONS_KEY, Security::Read) {
+		for value in reg.values() {
+            let key = value.as_ref().unwrap().name();
+            // This for some reason creates different size of vec
+            //let data: registry::Data = value.as_ref().unwrap().data().to_owned();
+            let data = &reg.value(key).unwrap();
+            if session_data.to_string() == data.to_string() {
+				println!("Current session is in session store under the name: {}", key.to_string_lossy());
+            };
 		}
 	}
-	
 }
